08b8eed6405ad88d0c500fc890a726bf51c3a227
Nina Loeck <n.ginczig@gmail.com>
2024-01-06 19:55:31 +0100

    extend console output, change back to class and main programming

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System;
+
+namespace HalloWelt
+{
+    class Programm
+    {
+
+        static void Main(string[] argd)
+        {
+            Console.WriteLine(@"Hallo Herr Meier, 
+Hiermit übersende ich Ihnen die Rechnung für meine Leistungen am 01.09.2021.
+Vielen Dankl, dass Die unsere Dienste genutzt haben.
+
+Mit freundlichen Grüßen
+Nina Loeck");
+        }
+
+    }
+
+}
+
+
