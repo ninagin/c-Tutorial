91d3ecfb32cf65a9a7030bd34e29535ba5f3b69d
Nina Loeck <n.ginczig@gmail.com>
2024-01-06 16:30:58 +0100

    Since -NETCore V.5.0 the console command can be written easier! Simplified code.

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello World!");
